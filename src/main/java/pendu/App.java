3a0c17144c0aaf63cf41cd08ad4a0bedf8718bad
Matthieu STEMPIN <stempin.matthieu@gmail.com>
2020-11-04 12:25:51 +0100

    start gameplay function

--- src/main/java/pendu/App.java
+++ src/main/java/pendu/App.java
@@ -1,4 +1,5 @@
 package pendu;
+import java.util.*;
 
 /**
  * Hello world!
@@ -6,12 +7,50 @@ package pendu;
 public final class App {
     private App() {
     }
+    public static String word(){
+        return "WORD";
+    }
 
-    /**
-     * Says hello to the world.
-     * @param args The arguments of the program.
-     */
+    public static boolean checkLetter(String word, String letterTry) {
+        for(int i = 0; i < word.length(); i++){
+            if (word.charAt(i) == Character.toUpperCase(letterTry.charAt(0))){
+                return (true);
+            }
+        }
+        return (false);
+    }
+
+    public static boolean game(String word){
+        boolean win = false;
+        boolean lose = false;
+        int attempt = 7;
+
+        Scanner scanner = new Scanner(System.in);
+        for(int i = 0; i < word.length(); i++){
+            System.out.printf("%s", "_ ");
+        }
+        while (!win && !lose){
+            System.out.println("Try to guess letter :");
+            String letterTry = scanner.nextLine();
+            if (checkLetter(word, letterTry)){
+                win = true;
+            }
+            else {
+                lose = true;
+            }
+        }
+        if (win){
+            System.out.println("You win !");
+            return (win);
+        }
+        else{
+            System.out.println("You lose !");
+            return (lose);
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String word = word();
+        game(word);
     }
 }
